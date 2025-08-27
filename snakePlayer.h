c715fe689e0993e8231e71f33d6517cbf650b006
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2025-08-27 10:39:56 -0300

    Main cpp created and fixed errors in my environment

--- snakePlayer.h
+++ snakePlayer.h
@@ -2,8 +2,12 @@
 #define SNAKEPLAYER.H
 
 class Snake{
+private:
     int food;
     bool life;
+public:
+    void eatFood(); //Responsable for eating food
+    bool isAlive(bool impacat); //Test for the impact of the Snake on itself and the borders    
 }
 
 
